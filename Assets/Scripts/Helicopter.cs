6274a6cecee7530e883b4eb4eb48b549c97f52a3
Jacob Salzberg <jacob1818@gmail.com>
2018-02-08 03:03:29 -0200

    Add Helicopter

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+
+    private bool called = false;
+    public AudioClip callSound;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("CallHeli") & !called) //done only once
+        {
+            called = true;
+            Debug.Log("Heli called");
+            audioSource.clip = callSound;
+            audioSource.Play();
+
+        }
+        else
+        {
+            
+        }
+    }
+}
