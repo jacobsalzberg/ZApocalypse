63491460ba5488ec13266683bfba11535b32ad59
Jacob Salzberg <jacob1818@gmail.com>
2018-02-08 01:40:51 -0200

    Add Respawn Script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public Transform playerSpawnPoints;
+    public Transform playerSpawnPoints; // the parent of the spawn points
+    public bool reSpawn = false;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
 
 	// Use this for initialization
 	void Start () {
-		
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        print(spawnPoints.Length); //check how many things get out --> gets 4
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (lastToggle != reSpawn)
+        {
+            Respawn();
+            reSpawn = false;
+        } else
+        {
+            lastToggle = reSpawn;
+        }
 	}
+
+    private void Respawn ()
+    {
+        int i = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[i].transform.position;
+    }
+
 }
